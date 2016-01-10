3469f60f72eefc48e31ddfe4a77b6b0550e41174
Sanmam18 <sanmam18@gmail.com>
2016-01-10 16:47:28 -0600

    my   math commit
    my math commit

---
+++ MathOperationsExamples.java
@@ -0,0 +1,71 @@
+package com.digitek.mathoperators;
+
+public class MathOperationsExamples {
+
+	/*
+	 * 
+	 * Math operators: +, -, *, / ,%
+	 *  
+	 */
+	
+	
+	public static void main(String[] args) {
+		addition();
+		substraction();
+		multiplication();
+		division();
+		modulus();
+	}
+	
+	public static void addition() {
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a + b;
+		System.out.println("addition() value of result:" + result);
+		
+	}
+	
+	public static void substraction() {
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a - b;
+		System.out.println("substraction() value of result:" + result);
+		
+	}
+	
+	public static void multiplication() {
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a * b;
+		System.out.println("multiplication value of result:" + result);
+		
+	}
+	
+	public static void division() {
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a / b;
+		System.out.println("division() value of result:" + result);
+		
+	}
+	
+	public static void modulus() {
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a + b;
+		System.out.println("modulus() value of result:" + result);
+		
+	}
+	
+
+}
